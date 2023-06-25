4746113479c28784d635d89da24d64e887fcdf82
adamhenson <adamhenson1979@gmail.com>
2023-06-25 18:28:22 -0400

    feat: tests

---
+++ src/lib/getModules.test.ts
@@ -0,0 +1,237 @@
+import { InputResult } from '../types';
+import { getModules } from './getModules';
+
+const mockResults: InputResult[] = [
+  {
+    bundleName: 'chunks/abc.123.js',
+    files: {
+      'node_modules/recoil/es/recoil.js': {
+        size: 65956,
+      },
+      '[sourceMappingURL]': {
+        size: 57,
+      },
+      '[unmapped]': {
+        size: 99,
+      },
+      '[EOLs]': {
+        size: 1,
+      },
+    },
+  },
+  {
+    bundleName: 'chunks/efg.456.js',
+    files: {
+      'node_modules/clsx/dist/clsx.m.js': {
+        size: 351,
+      },
+      'node_modules/react-is/cjs/react-is.production.min.js': {
+        size: 2117,
+      },
+      'node_modules/@babel/runtime/helpers/esm/defineProperty.js': {
+        size: 155,
+      },
+      'node_modules/@material-ui/core/esm/styles/transitions.js': {
+        size: 964,
+      },
+      'node_modules/@babel/runtime/helpers/esm/extends.js': {
+        size: 242,
+      },
+      'node_modules/@material-ui/core/esm/styles/colorManipulator.js': {
+        size: 2422,
+      },
+      'node_modules/@babel/runtime/helpers/esm/objectWithoutPropertiesLoose.js':
+        {
+          size: 172,
+        },
+      '[sourceMappingURL]': {
+        size: 89,
+      },
+      '[unmapped]': {
+        size: 101,
+      },
+      '[EOLs]': {
+        size: 1,
+      },
+    },
+  },
+  {
+    bundleName: 'chunks/hij.789.js',
+    files: {
+      'node_modules/prismjs/components/prism-json.js': {
+        size: 450,
+      },
+      '[no source]': {
+        size: 414,
+      },
+      'node_modules/prismjs/components/prism-jsx.js': {
+        size: 2434,
+      },
+      'node_modules/@material-ui/core/esm/styles/colorManipulator.js': {
+        size: 2422,
+      },
+      'helpers/loadScripts.ts': {
+        size: 268,
+      },
+      'helpers/usePrism.ts': {
+        size: 181,
+      },
+      'node_modules/prismjs/components/prism-typescript.js': {
+        size: 1258,
+      },
+      'node_modules/prismjs/components/prism-graphql.js': {
+        size: 2529,
+      },
+    },
+  },
+];
+
+describe('getModules', () => {
+  it('should return a module collection', () => {
+    const result = getModules({
+      inputResults: mockResults,
+    });
+    expect(result).toEqual({
+      'node_modules/recoil/es/recoil.js': {
+        bundleDependants: ['chunks/abc.123.js'],
+        file: 'node_modules/recoil/es/recoil.js',
+        size: 65956,
+      },
+      'node_modules/prismjs/components/prism-graphql.js': {
+        bundleDependants: ['chunks/hij.789.js'],
+        file: 'node_modules/prismjs/components/prism-graphql.js',
+        size: 2529,
+      },
+      'node_modules/prismjs/components/prism-jsx.js': {
+        bundleDependants: ['chunks/hij.789.js'],
+        file: 'node_modules/prismjs/components/prism-jsx.js',
+        size: 2434,
+      },
+      'node_modules/@material-ui/core/esm/styles/colorManipulator.js': {
+        bundleDependants: ['chunks/efg.456.js', 'chunks/hij.789.js'],
+        file: 'node_modules/@material-ui/core/esm/styles/colorManipulator.js',
+        size: 2422,
+      },
+      'node_modules/react-is/cjs/react-is.production.min.js': {
+        bundleDependants: ['chunks/efg.456.js'],
+        file: 'node_modules/react-is/cjs/react-is.production.min.js',
+        size: 2117,
+      },
+      'node_modules/prismjs/components/prism-typescript.js': {
+        bundleDependants: ['chunks/hij.789.js'],
+        file: 'node_modules/prismjs/components/prism-typescript.js',
+        size: 1258,
+      },
+      'node_modules/@material-ui/core/esm/styles/transitions.js': {
+        bundleDependants: ['chunks/efg.456.js'],
+        file: 'node_modules/@material-ui/core/esm/styles/transitions.js',
+        size: 964,
+      },
+      'node_modules/prismjs/components/prism-json.js': {
+        bundleDependants: ['chunks/hij.789.js'],
+        file: 'node_modules/prismjs/components/prism-json.js',
+        size: 450,
+      },
+      'node_modules/clsx/dist/clsx.m.js': {
+        bundleDependants: ['chunks/efg.456.js'],
+        file: 'node_modules/clsx/dist/clsx.m.js',
+        size: 351,
+      },
+      'helpers/loadScripts.ts': {
+        bundleDependants: ['chunks/hij.789.js'],
+        file: 'helpers/loadScripts.ts',
+        size: 268,
+      },
+      'node_modules/@babel/runtime/helpers/esm/extends.js': {
+        bundleDependants: ['chunks/efg.456.js'],
+        file: 'node_modules/@babel/runtime/helpers/esm/extends.js',
+        size: 242,
+      },
+      'helpers/usePrism.ts': {
+        bundleDependants: ['chunks/hij.789.js'],
+        file: 'helpers/usePrism.ts',
+        size: 181,
+      },
+      'node_modules/@babel/runtime/helpers/esm/objectWithoutPropertiesLoose.js':
+        {
+          bundleDependants: ['chunks/efg.456.js'],
+          file: 'node_modules/@babel/runtime/helpers/esm/objectWithoutPropertiesLoose.js',
+          size: 172,
+        },
+      'node_modules/@babel/runtime/helpers/esm/defineProperty.js': {
+        bundleDependants: ['chunks/efg.456.js'],
+        file: 'node_modules/@babel/runtime/helpers/esm/defineProperty.js',
+        size: 155,
+      },
+    });
+  });
+  it('should return a module collection with only `node_modules`', () => {
+    const result = getModules({
+      inputResults: mockResults,
+      onlyNodeModules: true,
+    });
+    expect(result).toEqual({
+      'node_modules/recoil/es/recoil.js': {
+        bundleDependants: ['chunks/abc.123.js'],
+        file: 'node_modules/recoil/es/recoil.js',
+        size: 65956,
+      },
+      'node_modules/prismjs/components/prism-graphql.js': {
+        bundleDependants: ['chunks/hij.789.js'],
+        file: 'node_modules/prismjs/components/prism-graphql.js',
+        size: 2529,
+      },
+      'node_modules/prismjs/components/prism-jsx.js': {
+        bundleDependants: ['chunks/hij.789.js'],
+        file: 'node_modules/prismjs/components/prism-jsx.js',
+        size: 2434,
+      },
+      'node_modules/@material-ui/core/esm/styles/colorManipulator.js': {
+        bundleDependants: ['chunks/efg.456.js', 'chunks/hij.789.js'],
+        file: 'node_modules/@material-ui/core/esm/styles/colorManipulator.js',
+        size: 2422,
+      },
+      'node_modules/react-is/cjs/react-is.production.min.js': {
+        bundleDependants: ['chunks/efg.456.js'],
+        file: 'node_modules/react-is/cjs/react-is.production.min.js',
+        size: 2117,
+      },
+      'node_modules/prismjs/components/prism-typescript.js': {
+        bundleDependants: ['chunks/hij.789.js'],
+        file: 'node_modules/prismjs/components/prism-typescript.js',
+        size: 1258,
+      },
+      'node_modules/@material-ui/core/esm/styles/transitions.js': {
+        bundleDependants: ['chunks/efg.456.js'],
+        file: 'node_modules/@material-ui/core/esm/styles/transitions.js',
+        size: 964,
+      },
+      'node_modules/prismjs/components/prism-json.js': {
+        bundleDependants: ['chunks/hij.789.js'],
+        file: 'node_modules/prismjs/components/prism-json.js',
+        size: 450,
+      },
+      'node_modules/clsx/dist/clsx.m.js': {
+        bundleDependants: ['chunks/efg.456.js'],
+        file: 'node_modules/clsx/dist/clsx.m.js',
+        size: 351,
+      },
+      'node_modules/@babel/runtime/helpers/esm/extends.js': {
+        bundleDependants: ['chunks/efg.456.js'],
+        file: 'node_modules/@babel/runtime/helpers/esm/extends.js',
+        size: 242,
+      },
+      'node_modules/@babel/runtime/helpers/esm/objectWithoutPropertiesLoose.js':
+        {
+          bundleDependants: ['chunks/efg.456.js'],
+          file: 'node_modules/@babel/runtime/helpers/esm/objectWithoutPropertiesLoose.js',
+          size: 172,
+        },
+      'node_modules/@babel/runtime/helpers/esm/defineProperty.js': {
+        bundleDependants: ['chunks/efg.456.js'],
+        file: 'node_modules/@babel/runtime/helpers/esm/defineProperty.js',
+        size: 155,
+      },
+    });
+  });
+});
